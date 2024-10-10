8f2a515508a4f13e2d4b6c06a0a1b149db82c208
Mathew Imanuel <107707513+MathewSin@users.noreply.github.com>
2024-10-10 12:01:46 +0700

    Update login.js
    Logic Reset input password if invalid username and password.

--- CaptchaStory/js/login.js
+++ CaptchaStory/js/login.js
@@ -28,6 +28,8 @@ function validateLogin(event) {
     } else {
         failedLoginAttempts++; // Increment failed login attempts
         errorMessage.textContent = 'Invalid username or password!';
+				  document.getElementById('username').value = '';
+        document.getElementById('password').value = '';
 
         // Check if failed attempts reached 5
         if (failedLoginAttempts >= 5) {
